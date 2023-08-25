e5d4eedb964550c8243ecf5a0b44b9995bc808c0
Eduardo Vieira <edgvi10@gmail.com>
2023-08-25 13:16:50 -0300

    Implementado máscara de data

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,3 +6,28 @@ export function mysqlDate(date) {
     // check if is Date object
     return (date && date instanceof Date) ? date.toISOString().slice(0, 19).replace('T', ' ') : new Date(date ?? "").toISOString().slice(0, 19).replace('T', ' ');
 }
+
+export function mysqlDateToJSDate(date) {
+    return new Date(date);
+}
+
+export function dateMask(mask, date) {
+    const date_object = (date && date instanceof Date) ? date : new Date(date ?? "");
+
+    const date_parts = {};
+    date_parts["Y"] = date_object.getFullYear();
+    date_parts["y"] = date_object.getFullYear().toString().slice(-2);
+    date_parts["m"] = (date_object.getMonth() + 1).toString().padStart(2, "0");
+    date_parts["d"] = date_object.getDate().toString().padStart(2, "0");
+    date_parts["H"] = date_object.getHours().toString().padStart(2, "0");
+    date_parts["h"] = (date_object.getHours() % 12).toString().padStart(2, "0");
+    date_parts["i"] = date_object.getMinutes().toString().padStart(2, "0");
+    date_parts["s"] = date_object.getSeconds().toString().padStart(2, "0");
+
+    let date_string = mask;
+    for (const key in date_parts) {
+        date_string = date_string.replace(key, date_parts[key]);
+    }
+
+    return date_string;
+}
